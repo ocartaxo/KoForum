ad87748a6a038636c9adbd3458276da6f9beca5b
Otavio Cartaxo <otaviocartaxo@ext.cvccorp.com.br>
2022-05-09 18:05:46 -0300

    injecao de dependencia dos mappers

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -3,39 +3,40 @@ package br.com.alura.forum.service
 import br.com.alura.forum.dtos.TopicoRequest
 import br.com.alura.forum.dtos.TopicoResponse
 import br.com.alura.forum.dtos.TopicoUpdate
-import br.com.alura.forum.mapper.RequestTopico
-import br.com.alura.forum.mapper.ResponseTopico
 import br.com.alura.forum.domain.Topico
-import br.com.alura.forum.dtos.RespotaTopicoResponse
+import br.com.alura.forum.dtos.RespostaTopicoResponse
 import br.com.alura.forum.exception.NotFoundException
+import br.com.alura.forum.mapper.TopicoMapper
 import br.com.alura.forum.repository.TopicoRepository
+import org.mapstruct.factory.Mappers
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.stream.Collectors
 
 @Service
 class TopicoService(
     private val repository: TopicoRepository,
-    private val responseTopico: ResponseTopico,
-    private val requestTopico: RequestTopico,
     private val notFoundMessage: String = "Tópico não encontrado!"
 ) {
+
+    private val mapper = Mappers.getMapper(TopicoMapper::class.java)
 
     fun listar(): List<TopicoResponse> {
 
         return repository.findAll().stream().map { t ->
-            responseTopico.map(t)
+            mapper.map(t)
         }.collect(Collectors.toList())
     }
 
     fun buscaPorId(id: Long): TopicoResponse {
         val t = repository.findById(id).orElseThrow { NotFoundException(notFoundMessage) }
-        return responseTopico.map(t)
+        return mapper.map(t)
     }
 
     fun cadastrar(t: TopicoRequest): TopicoResponse {
-        val topico: Topico = requestTopico.map(t)
+        val topico: Topico = mapper.map(t)
         repository.save(topico)
-        return responseTopico.map(topico)
+        return mapper.map(topico)
     }
 
     fun atualizar(t: TopicoUpdate): TopicoResponse {
@@ -44,16 +45,16 @@ class TopicoService(
         topico.titulo = t.titulo
         topico.mensagem = t.mensagem
 
-        return responseTopico.map(topico)
+        return mapper.map(topico)
     }
 
     fun deletar(id: Long) {
         repository.deleteById(id)
     }
 
-    fun buscaRespostas(id: Long): List<RespotaTopicoResponse> {
-        val rps = repository.findById(id).orElseThrow { NotFoundException(notFoundMessage) }.respostas
-        return rps.map { r -> responseTopico.map(r) }
+    fun buscaRespostas(id: Long): List<RespostaTopicoResponse> {
+        val respostas = repository.findById(id).orElseThrow { NotFoundException(notFoundMessage) }.respostas
+        return respostas.map { r -> mapper.map(r) }
     }
 
 
--- src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
+++ src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
@@ -2,28 +2,29 @@ package br.com.alura.forum.service
 
 import br.com.alura.forum.dtos.UsuarioResponse
 import br.com.alura.forum.exception.NotFoundException
-import br.com.alura.forum.mapper.ResponseUsuario
+import br.com.alura.forum.mapper.UsuarioMapper
 import br.com.alura.forum.repository.UsuarioRepository
+import org.mapstruct.factory.Mappers
 import org.springframework.stereotype.Service
 import java.util.stream.Collectors
 
 @Service
 class UsuarioService(
     private val repository: UsuarioRepository,
-    private val responseUsuario: ResponseUsuario,
     private val notFoundMessage: String = "Usuário não encontrado!"
 ) {
 
+    private val mapper = Mappers.getMapper(UsuarioMapper::class.java)
 
     fun listar(): List<UsuarioResponse> {
         return repository.findAll().stream().map { u ->
-            responseUsuario.map(u)
+            mapper.map(u)
         }.collect(Collectors.toList())
     }
 
     fun buscarPorId(id: Long): UsuarioResponse {
         val u = repository.findById(id).orElseThrow { NotFoundException(notFoundMessage) }
-        return responseUsuario.map(u)
+        return mapper.map(u)
     }
 
 }
