8345509b07c422a361b734856216d2da0336486d
Otavio Cartaxo <otaviocartaxo@ext.cvccorp.com.br>
2022-05-09 18:07:15 -0300

    adiciona o mapstruct & configura versão das dependencias/plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,29 +8,39 @@ plugins {
 	id("org.springframework.boot") version "2.6.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id ("org.jetbrains.kotlin.plugin.jpa") version "1.6.10"
-	id("org.jetbrains.kotlin.plugin.noarg") version "1.6.10"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+//	id("org.jetbrains.kotlin.plugin.noarg") version "1.6.10"
+	kotlin("jvm") version "1.5.10"
+	kotlin("plugin.spring") version "1.5.10"
+	kotlin("kapt") version "1.5.10"
 }
 
 group = "br.com.alura"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.10")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.10")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
+
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	val h2Version = "2.1.212"
+	runtimeOnly("com.h2database:h2:$h2Version")
+	testImplementation("com.h2database:h2:$h2Version")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	val mapstructVersion = "1.4.2.Final"
+	api("org.mapstruct:mapstruct:$mapstructVersion")
+	kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
 
 }
 
